e5fc7806d90d908a2a7e7ddca1030f7383534988
TilmeezUrRehmanBhatti <80107049+TilmeezUrRehmanBhatti@users.noreply.github.com>
2022-09-22 17:52:33 +0200

    JSON Jackson Demo - Nested Objects and Arrays

--- src/main/java/com/tilmeez/jackson/json/demo/Driver.java
+++ src/main/java/com/tilmeez/jackson/json/demo/Driver.java
@@ -13,7 +13,7 @@ public class Driver {
 
             // read JSON file and map/convert to java POJO data/sample-lite.json
             Student theStudent = mapper.readValue(
-                    new File("data/sample-lite.json"), Student.class);
+                    new File("data/sample-full.json"), Student.class);
 
             // print first name and last name
             System.out.println("First name: " + theStudent.getFirstName());
