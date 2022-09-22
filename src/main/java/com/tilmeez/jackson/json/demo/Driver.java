df2cf7da5c8058771d08799de8bdc13fa329e071
TilmeezUrRehmanBhatti <80107049+TilmeezUrRehmanBhatti@users.noreply.github.com>
2022-09-22 17:45:56 +0200

    JSON Jackson Demo - Processing JSON

---
+++ src/main/java/com/tilmeez/jackson/json/demo/Driver.java
@@ -0,0 +1,26 @@
+package com.tilmeez.jackson.json.demo;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+
+public class Driver {
+    public static void main(String[] args) {
+        try {
+
+            // create object mapper
+            ObjectMapper mapper = new ObjectMapper();
+
+            // read JSON file and map/convert to java POJO data/sample-lite.json
+            Student theStudent = mapper.readValue(
+                    new File("data/sample-lite.json"), Student.class);
+
+            // print first name and last name
+            System.out.println("First name: " + theStudent.getFirstName());
+            System.out.println("Last name: " + theStudent.getLastName());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
